54bbc041521791a02f42b66ee5d5562cfe507843
MuhamadRifkii <mrifkibhae@gmail.com>
2024-12-11 20:21:38 +0700

    fix(api): disable mongodb

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001;
 const routers = require("./routes");
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.mongodb);
+// mongoose.connect(process.env.mongodb);
 
 app.use(express.json());
 app.use(morgan("combined"));
